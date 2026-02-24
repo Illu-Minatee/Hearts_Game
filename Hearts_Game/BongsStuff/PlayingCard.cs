3d2d99214aa9d523c6ca23f253c8f03cf259d833
Michael <michael.millar@dcmail.ca>
2026-02-23 19:13:15 -0500

    Added Card Class

---
+++ Hearts_Game/BongsStuff/PlayingCard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hearts_Game.BongsStuff
+{
+    internal class PlayingCard
+    {
+        public int value { get; set; } = 0;
+        public bool isFaceUp { get; set; } = false;
+    }
+}
