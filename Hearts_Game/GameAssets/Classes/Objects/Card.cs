d087c220439ed1e10fa621e36ffc59b05d1a203d
Michael <michael.millar@dcmail.ca>
2026-02-23 20:59:09 -0500

    Added Card Assets

---
+++ Hearts_Game/GameAssets/Classes/Objects/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hearts_Game.GameAssets.Classes.Objects
+{
+    internal class Card
+    {
+        public int Value { get; set; } = 0;
+        public bool IsFaceUp { get; set; } = false;
+
+        public Card() { }
+
+        public Card(int value, bool faceUp = false)
+        {
+            Value = value;
+            IsFaceUp = faceUp;
+        }
+
+        public void Flip()
+        {
+            IsFaceUp = !IsFaceUp;
+        }
+    }
+}
---
+++ Hearts_Game/GameAssets/Classes/Objects/Deck.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hearts_Game.GameAssets.Classes.Objects
+{
+    internal class Deck
+    {
+    }
+}
